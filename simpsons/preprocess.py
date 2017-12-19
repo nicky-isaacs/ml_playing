4cfe60e1ba586435c6a893955f445ffd1d80ddbf
Alex Beal <alexlbeal@gmail.com>
2017-12-19 10:56:45 -0700

    Serialize simpsons images to tfdata.
    reformat simpsons_classifier

--- simpsons/preprocess.py
+++ simpsons/preprocess.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from typing import Iterator, Tuple, Generator
+from typing import Iterator, Tuple, Generator, List
 
 import tensorflow as tf
 import sys
@@ -8,17 +8,16 @@ import sys
 
 def files_gen(annotations_path: str) -> Generator[Tuple[str, int, int, int, int, str], None, None]:
     with open(annotations_path, 'rt') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')  # type: Iterator[Tuple[str, str, str, str, str, str]]
-        for row in spamreader:
+        annotation_reader = csv.reader(csvfile, delimiter=',')  # type: Iterator[Tuple[str, str, str, str, str, str]]
+        for row in annotation_reader:
             relative_path = row[0]
             csv_base_path = os.path.dirname(annotations_path)
             image_path = os.path.abspath(os.path.join(csv_base_path, relative_path))
-            ret = (image_path.encode('utf-8'),
-                   int(row[1]), int(row[2]),  # upper left x,y
-                   int(row[3]), int(row[4]),  # lower right x,y
-                   row[5].encode('utf-8'))
-            print(ret)
-            yield ret
+            if os.path.exists(image_path):
+                yield (image_path.encode('utf-8'),
+                       int(row[1]), int(row[2]),  # upper left x,y
+                       int(row[3]), int(row[4]),  # lower right x,y
+                       row[5].encode('utf-8'))
 
 
 def read_crop_resize(path, x1, y1, x2, y2, label):
@@ -51,10 +50,45 @@ def encode_and_save(image, filename):
     return tf.write_file(filename, encoded_jpg)
 
 
+def serialize_to_example(image, label, one_hot):
+    batch, height, width, channels = image.shape
+    return tf.train.Example(
+        features=tf.train.Features(feature={
+            'image': tf.train.Feature(float_list=tf.train.FloatList(value=image.flatten())),
+            'label': tf.train.Feature(int64_list=tf.train.Int64List(value=list(label))),
+            'one_hot': tf.train.Feature(float_list=tf.train.FloatList(value=one_hot.flatten())),
+            'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
+            'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
+            'channels': tf.train.Feature(int64_list=tf.train.Int64List(value=[channels])),
+        })
+    )
+
+
+def all_labels(all_annotations: List[Tuple[str, int, int, int, int, str]]) -> List[str]:
+    label_set = set()
+
+    for _, _, _, _, _, label in all_annotations:
+        label_set.add(label.strip())
+
+    label_list = list(label_set)
+    label_list.sort()
+
+    return label_list
+
+
+def make_one_hot(all_labels: List[str], label: tf.Tensor):
+    get_index_op = tf.py_func(all_labels.index, [label], tf.int64)
+    return tf.one_hot(get_index_op, len(all_labels))
+
+
 def process(annotations_path: str, output_path: str) -> None:
+    all_annotations = list(files_gen(annotations_path))
+
+    labels_list = all_labels(all_annotations)
+
     # Create a dataset of filenames, bounding boxes, and string labels
     filenames = tf.data.Dataset.from_generator(
-        lambda: files_gen(annotations_path),
+        lambda: (i for i in all_annotations),
         (tf.string,
          tf.int32, tf.int32,
          tf.int32, tf.int32,
@@ -64,21 +98,47 @@ def process(annotations_path: str, output_path: str) -> None:
     # Read and crop them on EIGHT THREADS HECK YEAH
     image_iter = filenames \
         .map(read_crop_resize, num_parallel_calls=8) \
+        .map(lambda img_, label_: (img_, label_, make_one_hot(labels_list, label_)), num_parallel_calls=8) \
         .make_one_shot_iterator()  # type: tf.data.Iterator
 
-    img, label = image_iter.get_next()
+    img_op, label_op, one_hot_op = image_iter.get_next()
 
-    i = 0
+    bar = ProgressBar(len(all_annotations))
     sess = tf.Session()
+    writer = tf.python_io.TFRecordWriter(output_path)
     while True:
         try:
             # This just writes the files back to disk.
             # TODO: serialize the processed images to TF/numpy binary format and save those along with the labels
-            op = encode_and_save(img, output_path + "/" + label + str(i) + ".jpg")
-            sess.run(op)
-            i += 1
+            # op = encode_and_save(img, output_path + "/" + label + str(i) + ".jpg")
+            img, label, one_hot_ = sess.run([img_op, label_op, one_hot_op])
+            serialized = serialize_to_example(img, label, one_hot_)
+            writer.write(serialized.SerializeToString())
+            bar.incr()
+            bar.display()
         except tf.errors.OutOfRangeError:
             break
+
+    writer.flush()
+    writer.close()
+
+
+class ProgressBar:
+    def __init__(self, total_items: int):
+        self.total_items = total_items
+        self.count = 0
+
+    def incr(self, count: int = 1) -> None:
+        self.count += count
+
+    def display(self) -> None:
+        msg = self.message()
+        msg_len = len(msg)
+        sys.stderr.write(("\b" * msg_len) + self.message())
+        sys.stderr.flush()
+
+    def message(self) -> str:
+        return "%d/%d" % (self.count, self.total_items)
 
 
 if __name__ == "__main__":
--- simpsons/simpsons_classifier.py
+++ simpsons/simpsons_classifier.py
@@ -10,10 +10,12 @@ IMAGE_HEIGHT_PIXELS = 300
 IMAGE_WIDTH_PIXELS = 200
 TOTAL_PIXELS = 300 * 200 * 3
 
+
 class BoundingBox:
     def __init__(self, upper_left_corner, lower_right_corner):
         self.upper_left_corner = upper_left_corner
         self.lower_right_corner = lower_right_corner
+
 
 class Label:
     def __init__(self, name, index, total_label_count):
@@ -21,19 +23,21 @@ class Label:
         self.index = index
         self.total_label_count = total_label_count
 
+
 class CroppedAndResizedSimpsonsImage:
     def __init__(self, image, label):
         self.image = image
         self.label = label
-    
+
     # Creates a tensor which is a vector of all the flattened RGB values in the image
     def to_tensor(self):
         # return a tensor of the image in shape [200*300*3]
-        image_data=[] 
+        image_data = []
         for pixel in self.image.getdata():
             r, g, b = pixel
-            image_data += [r,g,b]
+            image_data += [r, g, b]
         return tf.constant(image_data, tf.float64)
+
 
 class SimpsonsCSVEntry:
     def __init__(self, absolute_file_path, bounding_box, label):
@@ -41,10 +45,13 @@ class SimpsonsCSVEntry:
         self.bounding_box = bounding_box
         self.label = label
 
+
 def crop_and_resize(img, bounding_box):
-    return img\
-        .crop((bounding_box.upper_left_corner[0], bounding_box.upper_left_corner[1], bounding_box.lower_right_corner[0], bounding_box.lower_right_corner[1]))\
+    return img \
+        .crop((bounding_box.upper_left_corner[0], bounding_box.upper_left_corner[1], bounding_box.lower_right_corner[0],
+               bounding_box.lower_right_corner[1])) \
         .resize((IMAGE_HEIGHT_PIXELS, IMAGE_WIDTH_PIXELS), Image.ANTIALIAS)
+
 
 def simpons_csv_to_cropped_or_none(simpsons_csv):
     try:
@@ -56,13 +63,14 @@ def simpons_csv_to_cropped_or_none(simpsons_csv):
                 crop_and_resize(image, simpsons_csv.bounding_box),
                 simpsons_csv.label)
         else:
-                return None
+            return None
     except Exception as e:
         # print "Error:" + str(e)
         traceback.print_exc()
         return None
 
-# Format: filepath,x1,y1,x2,y2,character 
+
+# Format: filepath,x1,y1,x2,y2,character
 def read_csv(path):
     with open(path, 'rt') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -70,16 +78,19 @@ def read_csv(path):
             relative_path = row[0]
             csv_base_path = os.path.dirname(path)
             image_path = os.path.abspath(os.path.join(csv_base_path, relative_path))
-            yield SimpsonsCSVEntry(image_path, BoundingBox((int(row[1]), int(row[2])), (int(row[3]), int(row[4]))), row[5])
+            yield SimpsonsCSVEntry(image_path, BoundingBox((int(row[1]), int(row[2])), (int(row[3]), int(row[4]))),
+                                   row[5])
+
 
 def sort_and_uniq_labels(names):
-    as_set=list(set(names))
+    as_set = list(set(names))
     as_set.sort()
     return as_set
 
+
 if __name__ == "__main__":
-    csv_entries=[]
-    label_strings=[]
+    csv_entries = []
+    label_strings = []
     for csv_entry in read_csv(sys.argv[1]):
         entry = simpons_csv_to_cropped_or_none(csv_entry)
         if entry is not None:
@@ -88,7 +99,7 @@ if __name__ == "__main__":
         else:
             print(csv_entry)
     labels = sort_and_uniq_labels(label_strings)
-    total_labels=len(labels)
+    total_labels = len(labels)
     for e in csv_entries:
         label_index = labels.index(e.label)
         vec = np.zeros(total_labels)
@@ -121,7 +132,7 @@ if __name__ == "__main__":
     tf.global_variables_initializer().run()
     # Train
     inputs = []
-    true_values=[]
+    true_values = []
     for img in csv_entries:
         inputs += [img.to_tensor()]
         true_values += [img.one_hot]
