3578372d6f750f0e1ded640c70992ffedcbf3ae3
Alex Beal <alexlbeal@gmail.com>
2017-12-19 14:19:44 -0700

    Training loop runs \o/
    - preprocess now outputs two datasets: one for training and one for testing
    - classifier read, parses, trains, and prints accuracy

--- simpsons/preprocess.py
+++ simpsons/preprocess.py
@@ -4,6 +4,9 @@ from typing import Iterator, Tuple, Generator, List
 
 import tensorflow as tf
 import sys
+import random
+
+from ProgressBar import ProgressBar
 
 
 def files_gen(annotations_path: str) -> Generator[Tuple[str, int, int, int, int, str], None, None]:
@@ -50,16 +53,18 @@ def encode_and_save(image, filename):
     return tf.write_file(filename, encoded_jpg)
 
 
-def serialize_to_example(image, label, one_hot):
-    batch, height, width, channels = image.shape
+def serialize(image, label, one_hot):
+    image_string = tf.serialize_tensor(image)
+    one_hot_string = tf.serialize_tensor(one_hot)
+
+    return image_string, label, one_hot_string
+
+def make_example(image_string, label, one_hot_string):
     return tf.train.Example(
         features=tf.train.Features(feature={
-            'image': tf.train.Feature(float_list=tf.train.FloatList(value=image.flatten())),
-            'label': tf.train.Feature(int64_list=tf.train.Int64List(value=list(label))),
-            'one_hot': tf.train.Feature(float_list=tf.train.FloatList(value=one_hot.flatten())),
-            'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
-            'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
-            'channels': tf.train.Feature(int64_list=tf.train.Int64List(value=[channels])),
+            'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_string])),
+            'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label])),
+            'one_hot': tf.train.Feature(bytes_list=tf.train.BytesList(value=[one_hot_string])),
         })
     )
 
@@ -81,7 +86,7 @@ def make_one_hot(all_labels: List[str], label: tf.Tensor):
     return tf.one_hot(get_index_op, len(all_labels))
 
 
-def process(annotations_path: str, output_path: str) -> None:
+def process(annotations_path: str, output_train_path: str, output_test_path: str) -> None:
     all_annotations = list(files_gen(annotations_path))
 
     labels_list = all_labels(all_annotations)
@@ -99,50 +104,40 @@ def process(annotations_path: str, output_path: str) -> None:
     image_iter = filenames \
         .map(read_crop_resize, num_parallel_calls=8) \
         .map(lambda img_, label_: (img_, label_, make_one_hot(labels_list, label_)), num_parallel_calls=8) \
+        .map(serialize, num_parallel_calls=8)\
         .make_one_shot_iterator()  # type: tf.data.Iterator
 
     img_op, label_op, one_hot_op = image_iter.get_next()
 
     bar = ProgressBar(len(all_annotations))
-    sess = tf.Session()
-    writer = tf.python_io.TFRecordWriter(output_path)
-    while True:
-        try:
-            # This just writes the files back to disk.
-            # TODO: serialize the processed images to TF/numpy binary format and save those along with the labels
-            # op = encode_and_save(img, output_path + "/" + label + str(i) + ".jpg")
-            img, label, one_hot_ = sess.run([img_op, label_op, one_hot_op])
-            serialized = serialize_to_example(img, label, one_hot_)
-            writer.write(serialized.SerializeToString())
-            bar.incr()
-            bar.display()
-        except tf.errors.OutOfRangeError:
-            break
+    with tf.Session() as sess:
+        writer_train = tf.python_io.TFRecordWriter(output_train_path)
+        writer_test = tf.python_io.TFRecordWriter(output_test_path)
+        while True:
+            try:
+                # This just writes the files back to disk.
+                # TODO: serialize the processed images to TF/numpy binary format and save those along with the labels
+                # op = encode_and_save(img, output_path + "/" + label + str(i) + ".jpg")
+                img, label, one_hot_ = sess.run([img_op, label_op, one_hot_op])
+                serialized = make_example(img, label, one_hot_).SerializeToString()
+                if random.random() < 0.9:
+                    writer_train.write(serialized)
+                else:
+                    writer_test.write(serialized)
+                bar.incr()
+                bar.display()
+            except tf.errors.OutOfRangeError:
+                break
 
-    writer.flush()
-    writer.close()
-
-
-class ProgressBar:
-    def __init__(self, total_items: int):
-        self.total_items = total_items
-        self.count = 0
-
-    def incr(self, count: int = 1) -> None:
-        self.count += count
-
-    def display(self) -> None:
-        msg = self.message()
-        msg_len = len(msg)
-        sys.stderr.write(("\b" * msg_len) + self.message())
-        sys.stderr.flush()
-
-    def message(self) -> str:
-        return "%d/%d" % (self.count, self.total_items)
+        writer_train.flush()
+        writer_train.close()
+        writer_test.flush()
+        writer_test.close()
 
 
 if __name__ == "__main__":
     annotations_path = sys.argv[1]  # Path to annotation.txt
-    output_path = sys.argv[2]  # Path to output location
+    output_train_path = sys.argv[2]  # Path to output location for training
+    output_test_path = sys.argv[3]  # Path to output location for testing
 
-    process(annotations_path, output_path)
+    process(annotations_path, output_train_path, output_test_path)
