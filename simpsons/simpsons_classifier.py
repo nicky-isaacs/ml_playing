0fb93631e302285535025b5c19304ff6827cdeec
Nicholas Isaacs <nisaacs@Nicholass-MBP.domain>
2017-12-21 10:12:47 -0700

    WIP: working on sorting the images after prediction

--- simpsons/simpsons_classifier.py
+++ simpsons/simpsons_classifier.py
@@ -15,15 +15,22 @@ def deserialize_example(example_bytes):
         features={
             'image': tf.FixedLenFeature([], dtype=tf.string),
             'one_hot': tf.FixedLenFeature([], dtype=tf.string),
+            'label': tf.FixedLenFeature([], dtype=tf.string),
+            'image_path': tf.FixedLenFeature([], dtype=tf.string),
         },
         name='parse_example'
     )
 
     parsed_img_tensor = tf.parse_tensor(parsed['image'], tf.float32, name='parse_image')
     parsed_one_hot_tensor = tf.parse_tensor(parsed['one_hot'], tf.float32, name='parse_one_hot')
+    parsed_label = parsed['label']
+    parsed_path = parsed['image_path']
 
-    return tf.reshape(parsed_img_tensor, (TOTAL_PIXELS,)), tf.reshape(parsed_one_hot_tensor, (TOTAL_CLASSES,))
+    return tf.reshape(parsed_img_tensor, (TOTAL_PIXELS,)), tf.reshape(parsed_one_hot_tensor, (TOTAL_CLASSES,)), parsed_label, parsed_path 
+from shutil import import copyfile
 
+def put_in_predicted_location(location, from_file):
+    copyfile(f,     
 
 if __name__ == "__main__":
     dataset = tf.data.TFRecordDataset(sys.argv[1])
@@ -35,7 +42,7 @@ if __name__ == "__main__":
         .batch(10)\
         .make_one_shot_iterator()  # type: tf.data.Iterator
 
-    inputs_op, true_values_op = data_iter.get_next()
+    inputs_op, true_values_op, label_op, path = data_iter.get_next()
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, TOTAL_PIXELS])
@@ -65,7 +72,7 @@ if __name__ == "__main__":
     steps = 1000
     bar = ProgressBar(steps)
     for _ in range(steps):
-        inputs, true_values = sess.run([inputs_op, true_values_op])
+        inputs, true_values, label = sess.run([inputs_op, true_values_op])
         sess.run(train_step, feed_dict={x: inputs, y_: true_values})
         bar.incr()
         bar.display()
@@ -74,18 +81,20 @@ if __name__ == "__main__":
 
     data_iter_test = dataset_test\
         .map(deserialize_example, num_parallel_calls=8)\
-        .repeat()\
-        .shuffle(1000)\
-        .batch(1000)\
         .make_one_shot_iterator()  # type: tf.data.Iterator
 
-    inputs_op, true_values_op = data_iter_test.get_next()
+    inputs_op, true_values_op, label_op, path_op = data_iter_test.get_next()
 
     # Test trained model
-    softmax_op = tf.nn.softmax(y, 1)
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    inputs, true_values = sess.run([inputs_op, true_values_op])
-    accuracy, our_prediction = sess.run([accuracy_op, softmax_op], feed_dict={x: inputs, y_: true_values})
-    print("\nTest accuracy: %f" % accuracy)
-    print(our_prediction)
+    while True:
+        try:
+            softmax_op = tf.nn.softmax(y, 1)
+            predicted_position = tf.argmax(y, 1)
+            correct_prediction = tf.equal(predicted_position, tf.argmax(y_, 1))
+            accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            inputs, true_values, label, path = sess.run([inputs_op, true_values_op, label_op, path_op])
+            accuracy, our_prediction = sess.run([accuracy_op, softmax_op], feed_dict={x: inputs, y_: true_values})
+            print("\nTest accuracy: %f" % accuracy)
+            print(our_prediction)
+        except tf.errors.OutOfRangeError:
+            break
