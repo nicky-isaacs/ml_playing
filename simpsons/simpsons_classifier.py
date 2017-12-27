6b93af292a833c5e06be95391b36c6b24e3517e2
Nicholas Isaacs <nisaacs@Nicholass-MBP.domain>
2017-12-26 22:41:36 -0700

    Added training accuracy metric

--- simpsons/simpsons_classifier.py
+++ simpsons/simpsons_classifier.py
@@ -1,8 +1,10 @@
-import tensorflow as tf
-import sys
 import argparse
-from shutil import copyfile, rmtree
 import os
+import numpy as np
+from shutil import copyfile, rmtree
+
+import tensorflow as tf
+
 from ProgressBar import ProgressBar
 
 IMAGE_HEIGHT_PIXELS = 300
@@ -13,13 +15,17 @@ TOTAL_CLASSES = 18
 
 class Flags:
 
-    def __init__(self, prediction_output_base_dir: str, max_steps: int, training_data: str, test_data: str, learn_rate: float, logs_dir: str):
+    def __init__(self, prediction_output_base_dir: str, max_steps: int, training_data: str, test_data: str,
+                 learn_rate: float,
+                 logs_dir: str, tf_summary_interval: int,):
         self.prediction_output_base_dir = prediction_output_base_dir
         self.max_steps = max_steps
         self.training_data = training_data
         self.test_data = test_data,
-        self.learn_rate=learn_rate
-        self.logs_dir=logs_dir
+        self.learn_rate = learn_rate
+        self.logs_dir = logs_dir
+        self.tf_summary_interval = tf_summary_interval
+
 
 def parse_flags() -> Flags:
     parser = argparse.ArgumentParser()
@@ -29,7 +35,7 @@ def parse_flags() -> Flags:
                         type=int,
                         default=1,
                         help="number of epochs to train (default 1)")
-    
+
     parser.add_argument('--prediction-dir',
                         dest='prediction_output_base_dir',
                         type=str,
@@ -58,16 +64,22 @@ def parse_flags() -> Flags:
                         default='/tmp/simpsons_logs',
                         help='where to log tensorflow data (default /tmp/simpsons_logs)')
 
+    parser.add_argument('--summary-interval',
+                        dest='summary_interval',
+                        type=int,
+                        default=100,
+                        help='how many training steps between tensorboard metrics are posted (default 100)')
 
     args = parser.parse_args()
 
-    all_args=[
+    all_args = [
         args.prediction_output_base_dir,
         args.steps,
         args.training_data,
         args.test_data,
         args.logs_dir,
         args.learn_rate,
+        args.summary_interval,
     ]
 
     # Ensure that all the flags are defined
@@ -83,7 +95,9 @@ def parse_flags() -> Flags:
         test_data=args.test_data,
         learn_rate=args.learn_rate,
         logs_dir=args.logs_dir,
+        tf_summary_interval=args.summary_interval,
     )
+
 
 def deserialize_example(example_bytes):
     parsed = tf.parse_single_example(
@@ -102,7 +116,9 @@ def deserialize_example(example_bytes):
     parsed_label = parsed['label']
     parsed_path = parsed['image_path']
 
-    return tf.reshape(parsed_img_tensor, (TOTAL_PIXELS,)), tf.reshape(parsed_one_hot_tensor, (TOTAL_CLASSES,)), parsed_label, parsed_path
+    return tf.reshape(parsed_img_tensor, (TOTAL_PIXELS,)), tf.reshape(parsed_one_hot_tensor,
+                                                                      (TOTAL_CLASSES,)), parsed_label, parsed_path
+
 
 def put_in_predicted_location(base_sort_dir, location, from_file):
     location = os.path.join(base_sort_dir, location)
@@ -114,17 +130,19 @@ def put_in_predicted_location(base_sort_dir, location, from_file):
     filename = os.path.basename(from_file)
     copyfile(from_file, os.path.join(location, filename))
 
-def variable_summaries(var):
-  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-  with tf.name_scope('summaries'):
-    mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean', mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
-    tf.summary.histogram('histogram', var)
+
+def variable_summaries(var: tf.Variable):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 
 def setup_tensorboard(w, b, cross_entropy):
     """Given the variables and tensors we care about, attach scoped metrics"""
@@ -134,30 +152,46 @@ def setup_tensorboard(w, b, cross_entropy):
         variable_summaries(b)
     tf.summary.scalar('cross entropy', cross_entropy)
 
+
+def create_test_dataset(flags: Flags):
+    dataset_test = tf.data.TFRecordDataset(flags.test_data)
+    return dataset_test \
+        .map(deserialize_example, num_parallel_calls=8) \
+        .batch(1) \
+        .make_one_shot_iterator()  # type: tf.data.Iterator
+
+
+def build_test_dataset(flags: Flags):
+    dataset_test = tf.data.TFRecordDataset(flags.test_data)
+    return dataset_test \
+        .map(deserialize_example, num_parallel_calls=8) \
+        .batch(1) \
+        .make_one_shot_iterator()  # type: tf.data.Iterator
+
+
 if __name__ == "__main__":
-    flags=parse_flags()
+    flags = parse_flags()
     try:
         rmtree(flags.prediction_output_base_dir)
         os.mkdir(flags.prediction_output_base_dir)
     except Exception:
         os.mkdir(flags.prediction_output_base_dir)
 
-
     dataset = tf.data.TFRecordDataset(flags.training_data)
 
-    data_iter = dataset\
-        .map(deserialize_example, num_parallel_calls=8)\
-        .repeat()\
-        .shuffle(1000)\
-        .batch(10)\
+    data_iter = dataset \
+        .map(deserialize_example, num_parallel_calls=8) \
+        .repeat() \
+        .shuffle(1000) \
+        .batch(10) \
         .make_one_shot_iterator()  # type: tf.data.Iterator
 
     inputs_op, true_values_op, label_op, path = data_iter.get_next()
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, TOTAL_PIXELS])
-    W: tf.Variable = tf.Variable(tf.zeros([TOTAL_PIXELS, TOTAL_CLASSES]))
-    b: tf.Variable = tf.Variable(tf.zeros([TOTAL_CLASSES]))
+    W = tf.Variable(tf.zeros([TOTAL_PIXELS, TOTAL_CLASSES]))  # type: tf.Variable
+    b = tf.Variable(tf.zeros([TOTAL_CLASSES]))  # type: tf.Variable
 
     # y = ax+b, learn a and b
     y = tf.matmul(x, W) + b
@@ -179,32 +213,45 @@ if __name__ == "__main__":
     train_step = tf.train.GradientDescentOptimizer(flags.learn_rate).minimize(cross_entropy)
 
     setup_tensorboard(W, b, cross_entropy)
-    merged = tf.summary.merge_all()
+
     sess = tf.InteractiveSession()
     train_writer = tf.summary.FileWriter(flags.logs_dir + '/train',
                                          sess.graph)
     test_writer = tf.summary.FileWriter(flags.logs_dir + '/test')
 
+    train_acc_report_var = tf.Variable(initial_value=0.0, dtype=tf.float32)
+
+    with tf.name_scope('training_accuracy'):
+        variable_summaries(train_acc_report_var)
+
+    acc_report_test_data = create_test_dataset(flags)
+    acc_inputs_op, acc_true_values_op, label_op, path_op = acc_report_test_data.get_next()
+
+    def update_train_acc():
+        predicted_position = tf.argmax(y, 1)
+        correct_prediction = tf.equal(predicted_position, tf.argmax(y_, 1))
+        acc_op = tf.reduce_mean(tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))
+        update_acc_op = train_acc_report_var.assign((acc_op + tf.convert_to_tensor(train_acc_report_var)) / tf.constant(2.0, tf.float32))
+        inputs, true_values = sess.run([inputs_op, true_values_op])
+        sess.run(update_acc_op, feed_dict={x: inputs, y_: true_values})
+
     tf.global_variables_initializer().run()
 
     bar=ProgressBar(0)
+    merged = tf.summary.merge_all()
     for i in range(flags.max_steps):
         try:
             inputs, true_values, label = sess.run([inputs_op, true_values_op, label_op])
             summary, _ = sess.run([merged,train_step], feed_dict={x: inputs, y_: true_values})
-            if (0== i%10):
+            if i%flags.tf_summary_interval is 0:
                 train_writer.add_summary(summary, i)
+                update_train_acc()
             bar.incr()
             bar.display()
         except tf.errors.OutOfRangeError:
             break
 
-    dataset_test = tf.data.TFRecordDataset(flags.test_data)
-
-    data_iter_test = dataset_test\
-        .map(deserialize_example, num_parallel_calls=8)\
-        .batch(1)\
-        .make_one_shot_iterator()  # type: tf.data.Iterator
+    data_iter_test = build_test_dataset(flags)
 
     inputs_op, true_values_op, label_op, path_op = data_iter_test.get_next()
 
